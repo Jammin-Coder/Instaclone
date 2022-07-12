f7186bd46dd9222d5e2b78f292cfcec194d0ace1
jammincoder <jammincoderguy96@gmail.com>
2022-07-12 15:53:44 -0400

    Profile page layout complete
    Next optimize the CSS

--- src/NavLogos.js
+++ src/NavLogos.js
@@ -1,12 +1,12 @@
 let dir = '/img/';
-let message = dir + '/message.png';
-let home = dir + '/home.png';
-let newPost = dir + '/new.png';
+let message = dir + 'message.png';
+let home = dir + 'home.png';
+let newPost = dir + 'new.png';
 let defaultSize = 24 + 'px';
 
 export function Message(props) {
     let size;
-    props.size ? size = props.size + 'px': size = 32 + 'px';
+    props.size ? size = props.size + 'px': size = defaultSize;
     return <a className='Navbar__link' href='/messages'>
         <img src={message} style={{width: size, height: size }}/>
     </a>
--- src/Profile/Profile.js
+++ src/Profile/Profile.js
@@ -15,20 +15,20 @@ export function ProfilePic(props) {
 export function ProfileBio(props) {
     return (
         <div className='ProfileBio'>
-            <div className='ProfileBio__header'>
-                <h2>jammin_coder</h2>
-                <button className='ProfileBio__edit'>Edit profile</button>
-                <a className='ProfileBio__settings' href='#settings'>Settings</a>
+            <div className='flex gap-5 [align-items:center]'>
+                <h2 className='text-3xl'>jammin_coder</h2>
+                <button className='btn'>Edit profile</button>
+                <a className='flex underline' href='#settings'>Settings</a>
             </div>
             
-            <div className='ProfileBio__stats'>
+            <div className='flex my-[2em] gap-5'>
                 <a href='#posts'><b>2</b> posts</a>
                 <a href='#followers'><b>17</b> followers</a>
                 <a href='#following'><b>15</b> following</a>
             </div>
 
             <div className='ProfileBio__main'>
-                <p className='ProfilBio__name'>Tim Batt</p>
+                <p className='font-bold'>Tim Batt</p>
                 I'm a young software developer and aspiring artist/designer.<br/>
                 <a href='https://timbattblog.com'>timbattblog.com</a>
             </div>
@@ -57,6 +57,18 @@ export function ProfileHeader(props) {
     );
 }
 
+export function Posts(props) {
+    return (
+        <div className='Posts grid grid-cols-3 gap-1'>
+            <img src='/img/posts/panda.png' className='aspect-square border border-gray-700'/>
+            <img src='/img/posts/panda.png' className='aspect-square border border-gray-700'/>
+            <img src='/img/posts/panda.png' className='aspect-square border border-gray-700'/>
+            
+            <img src='/img/posts/panda.png' className='aspect-square border border-gray-700'/>
+            
+        </div>
+    );
+}
 
 export default function Profile(props) {
     return (
@@ -64,6 +76,7 @@ export default function Profile(props) {
             <div className='Profile'>
                 <ProfileHeader />
                 <ProfileNav/>
+                <Posts/>
             </div>
         </div>
     );
